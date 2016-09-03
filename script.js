dfb7699a009e980781402b663f24e37b765b2161
sankit acharya <sankit.acharya@gmail.com>
2016-09-04 01:28:17 +0530

    Added multiple cats

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-$(document).ready(function(){
+var cats = ["Snoofy","Snow"];
+var click1=0;
+var click2 = 0;
 
-var click;
-$('.catImage').click(function(e) {
-  clicks += 1;
-  $('#score').text(clicks);
-});
-});
+  $('.cat_main').each(function() {
+    $(this).prepend("<h1>"+ cats.pop() +"</h1>");
+  });
+
+  $('.catImage1').click(function() {
+    click1 = click1 + 1;
+    $('#score1').text(click1);
+  });
+  $('.catImage2').click(function() {
+    click2 = click2 + 1;
+    $('#score2').text(click2);
+  });
