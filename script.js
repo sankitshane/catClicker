923cbfb2e39f936eb5ac732989ec857bf4c3f204
sankit acharya <sankit.acharya@gmail.com>
2016-09-04 14:35:37 +0530

    implimented MVO

--- script.js
+++ script.js
@@ -1,16 +1,148 @@
-var cats = ["Snoofy","Snow"];
-var click1=0;
-var click2 = 0;
+$(function() {
 
-  $('.cat_main').each(function() {
-    $(this).prepend("<h1>"+ cats.pop() +"</h1>");
-  });
+var model = {
+            current_cat : null,
+            cats : [
+              {"name":"Snoopy",
+                "url":"cat.jpg",
+                "score":0},
+              {"name":"Snow",
+                "url":"ncat.jpg",
+                "score":0},
+              {"name":"Cuddles",
+                "url":"cat1.jpg",
+                "score":0},
+              {"name":"snuggles",
+                "url":"cat2.jpeg",
+                "score":0},
+              {"name":"tiger",
+                "url":"tiger.png",
+                "score":0}
+              ]
+            };
 
-  $('.catImage1').click(function() {
-    click1 = click1 + 1;
-    $('#score1').text(click1);
-  });
-  $('.catImage2').click(function() {
-    click2 = click2 + 1;
-    $('#score2').text(click2);
-  });
+var octopus = {
+  init : function() {
+    model.current_cat = model.cats[0];
+    catListView.init();
+    catView.init();
+  },
+  getCurrentCat : function() {
+    return model.current_cat;
+  },
+  setCurrentCat : function(cat) {
+    model.current_cat = cat;
+  },
+  getCats : function() {
+    return model.cats;
+  },
+  incrementScore : function() {
+    model.current_cat.score++;
+    catView.render();
+  }
+};
+
+var catView = {
+  init : function() {
+    this.catElem = document.getElementById('cat');
+    this.catnameElem = document.getElementById('cat_name');
+    this.catImgElem = document.getElementById('cat_img');
+    this.catScoreElem = document.getElementById('cat_score');
+
+    this.catImgElem.addEventListener('click',function() {
+      octopus.incrementScore();
+    });
+    this.render();
+  },
+  render : function() {
+    var currentCat = octopus.getCurrentCat();
+    this.catnameElem.textContent = currentCat.name;
+    this.catImgElem.src = currentCat.url;
+    this.catScoreElem.textContent = currentCat.score;
+  }
+};
+
+var catListView = {
+  init : function() {
+    this.catListElem = document.getElementById('list_content');
+    this.render();
+  },
+  render : function() {
+    var cat,elem,i;
+
+    var cats = octopus.getCats();
+    this.catListElem.innerHTML = "";
+
+    for (var i = 0; i < cats.length; i++) {
+
+    // This is the number we're on...
+    cat = cats[i];
+
+    // We're creating a DOM element for the number
+    elem = document.createElement('li');
+    elem.textContent = cat.name;
+
+    // ... and when we click, alert the value of `num`
+    elem.addEventListener('click', (function(catCopy) {
+        return function() {
+            octopus.setCurrentCat(catCopy);
+            catView.render();
+        };
+    })(cat));
+      this.catListElem.appendChild(elem);
+  }
+}
+};
+
+octopus.init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+// Let's loop over the numbers in our array
+for (var i = 0; i < cats.length; i++) {
+
+// This is the number we're on...
+var cat = cats[i];
+
+// We're creating a DOM element for the number
+var elem = document.createElement('div');
+elem.textContent = cat.name;
+
+// ... and when we click, alert the value of `num`
+elem.addEventListener('click', (function(catCopy) {
+    return function() {
+
+        var catscreen = document.createElement('div');
+
+        catscreen.innerHTML += '<h2>'+catCopy.name +'</h2>';
+        catscreen.innerHTML += '<img src="'+catCopy.url+'" alt="A image of the cat" class="catImage">';
+        catCopy.score++;
+        score(catCopy.score);
+        //document.body.appendChild(catscreen);
+        $('.cats_space').append(catscreen);
+    };
+})(cat));
+
+//document.body.appendChild(elem);
+$('.list_content').append(elem);
+};
+function score(a){
+$('.score').text(a);
+};*/
+
+}());
